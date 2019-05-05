c8ad4a92131913350c839ba8ddbf37759b2dd263
RobHarrisAZ <monte96_@hotmail.com>
2019-05-05 13:35:20 -0700

    Add node-fetch, change license, pull google calendar data

--- index.js
+++ index.js
@@ -1,86 +1,96 @@
 require('dotenv').config();
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const { google } = require('googleapis');
-const gClientId = process.env.GAPI_CLIENT_ID;
-const gClientSec = process.env.GAPI_CLIENT_SECRET;
+const { Client, RichEmbed } = require('discord.js');
+const client = new Client();
+const fetch = require('node-fetch');
+// const { google } = require('googleapis');
+// const gClientId = process.env.GAPI_CLIENT_ID;
+// const gClientSec = process.env.GAPI_CLIENT_SECRET;
+// const scopes = 'https://www.googleapis.com/auth/calendar';
+// const cal = google.calendar({
+//     version: 'v3',
+//     auth: gApiKey
+// })
+
 const gApiKey = process.env.GAPI_KEY;
-const scopes = 'https://www.googleapis.com/auth/calendar';
-const cal = google.calendar({
-    version: 'v3',
-    auth: gApiKey
-})
-const params = {
-    calendarId: process.env.CALENDAR_ID
-};
+const calendarId = process.env.CALENDAR_ID;
+const weeklyMilliseconds = 604800000;
+
+let calendarData = null;
+let filteredEvents = [];
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        //msg.reply('Pong!');
-        //checkAuth();
-        getCalendarData();
+    if (msg.content === '!r') {
+        filteredEvents.length = 0;
+        getEventData('')
+            .then(function () {
+                filteredEvents.forEach(item => {
+                    const embed = new RichEmbed()
+                    // Set the title of the field
+                    .setTitle(item.summary)
+                    // Set the color of the embed
+                    .setColor(0x00FFFF)
+                    // Set the main content of the embed
+                    .setDescription(`Start Time: ${item.start.dateTime} \n${item.description}`);
+                  // Send the embed to the same channel as the message
+                    
+                    msg.channel.send(embed);
+    
+                })
+                const embed = new RichEmbed()
+                // Set the title of the field
+                .setTitle('Calendar Retrieved!')
+                // Set the color of the embed
+                .setColor(0x00FFFF)
+                // Set the main content of the embed
+                .setDescription(`Count: ${filteredEvents.length}`);
+              // Send the embed to the same channel as the message
+                msg.channel.send(embed);
+                //msg.reply('Calendar Retrieved!');
+                //msg.reply('Count: ' + filteredEvents.length);                    
+        })
     }
 });
 
 client.login(process.env.BOT_TOKEN);
 
-async function getCalendarData() {
-    handleAuthClick();
-    //const calData = await cal.calendars.get({ calendarId: params.calendarId });
-    //console.log(calData);
-    // gapi.client.load('calendar', 'v3', function() {
-    //     var request = gapi.client.calendar.events.list({
-    //       'calendarId': process.env.CALENDAR_ID
-    //     });
-              
-    //     request.execute(function(resp) {
-    //       for (var i = 0; i < resp.items.length; i++) {
-    //         var li = document.createElement('li');
-    //         li.appendChild(document.createTextNode(resp.items[i].summary));
-    //         document.getElementById('events').appendChild(li);
-    //       }
-    //     });
-    //   });
-};
-
-handleClientLoad = () => {
-    google.client.setApiKey(apiKey);
-    window.setTimeout(checkAuth, 1);
-    checkAuth();
-};
-  
-checkAuth = () => {
-    gapi.auth.authorize({ client_id: clientId, scope: scopes, immediate: true },
-        handleAuthResult);
-};
-  
-handleAuthResult = (authResult) => {
-    if (authResult) {
-        getCalendarData();
-    }
-};
-  
-handleAuthClick = (event) => {
-    google.auth.(
-         { clientId: gClientId, clientSecret: gClientSec, redirectUrl: '' },
-         handleAuthResult);
-    google.client.init({
-        'apiKey': gApiKey,
-        // clientId and scope are optional if auth is not required.
-        'clientId': gClientId,
-        'scope': 'profile',
-    }).then(function () {
-        // 3. Initialize and make the API request.
-        return google.client.request({
-            'path': 'https://calendar.googleapis.com/v3/calendar/' + params.calendarId,
-        })
-    }).then(function (response) {
-        console.log(response.result);
-    }, function (reason) {
-        console.log('Error: ' + reason.result.error.message);
+function getEventData(pageToken) {
+    return getCalendarData(pageToken)
+    .then(cal => {
+        calendarData = cal;
+        events = calendarData.items.filter(isFutureEvent);
+        filteredEvents = filteredEvents.concat(events);
+        if (calendarData.nextPageToken) {
+            return getEventData(calendarData.nextPageToken);
+        }
     });
+}
+
+function isFutureEvent(item) {
+    if (item.kind === 'calendar#event' && item.status !== 'cancelled') {
+        const now = new Date();
+        const eventDate = new Date(item.start.dateTime);
+        //console.log(now.getTime() + ' ' + eventDate.getTime());
+        return now.getTime() < eventDate.getTime();
+    } else {
+        return false;
+    }
+}
+function getCalendarData(pageToken) {
+    return fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${gApiKey}&pageToken=${pageToken}`,
+        {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
 };

35e89f64598b95898c83edd3ee440d5555d3e2ce
Infidel@BLUEBEAST <monte96_@hotmail.com>
2019-05-15 14:48:31 -0700

    Added pledges

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 require('dotenv').config();
 const CronJob = require('node-cron');
-const { Client, Channel, RichEmbed } = require('discord.js');
+const { Client, RichEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 const TurndownService = require('turndown');
-const { addDays, differenceInHours, differenceInMinutes, format, isSameDay } = require('date-fns');
+const { addDays, differenceInDays, differenceInMinutes, format, isSameDay } = require('date-fns');
 const bbConvert = require('bbcode-to-markdown');
+const googleCalendarListSort = require('google-calendar-by-date');
 
 const turndownService = new TurndownService();
 const client = new Client();
-const gApiKey = process.env.GAPI_KEY;
-const calendarId = process.env.CALENDAR_ID;
 
-let calendarData = null;
 let vCalendarData = null;
-let filteredEvents = [];
-let vFilteredEvents = [];
 let channelTargets = [process.env.CHANNEL1, process.env.CHANNEL2];
+let pledgeData = null;
+let pledgeItems = [];
+let esoData = null;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    loadData();
     loadEvents().then(() => {
         console.log(`Loaded events`);
+        //getPledges();
     });
     CronJob.schedule('10 0 * * *', function () {
         loadEvents().then(() => {
@@ -30,7 +31,7 @@ client.on('ready', () => {
     CronJob.schedule('*/15 * * * *', checkEvents, null, true, 'America/Los_Angeles');
     CronJob.schedule('0 3 * * *', function () {
         channelTargets.forEach(item => {
-            client.channels.get(item).send(getDayEvents(Date.now()));
+            client.channels.get(item).send(getDayEvents(Date.now()).concat(getDailyPledges(Date.now())));
         });
     }, null, true, 'America/Los_Angeles');
 });
@@ -41,7 +42,6 @@ client.on('message', msg => {
 
     }
     if (msg.content === '!refresh') {
-        vFilteredEvents.length = 0;
         loadEvents().then(() => {
             const embed2 = new RichEmbed()
                 .setTitle('Calendar Retrieved!')
@@ -84,6 +84,12 @@ client.on('message', msg => {
     if (msg.content.startsWith('!ttp')) {
         msg.channel.send('Coming soon');
     }
+    if (msg.content === '!pledges') {
+        msg.channel.send(new RichEmbed()
+        .setTitle(`Today's Pledges`)
+        .setColor(0x20F41F)
+        .setDescription(turndownService.turndown(getDailyPledges(Date.now()))));       
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
@@ -92,11 +98,8 @@ function loadEvents() {
     return getVEventData()
         .then(function () {
             vCalendarData.events.forEach(processEvents);
-            //vFilteredEvents = vCalendarData.events.filter(isFutureEvent);
-            //vFilteredEvents.sort(dateSort);
         });
 }
-
 function getChannelList() {
     let channelList = '';
     channelTargets.forEach(item => {
@@ -145,6 +148,9 @@ function getDayEvents(day) {
         description = description.concat(info);
     });
 
+    description = description.concat(getPledgeText()).concat(`<b>Pledges:</b><br/>`).concat(getDailyPledges(day));
+    //description = description.concat(getWeekly());
+
     return new RichEmbed()
         .setTitle(`${process.env.GUILD_NAME} Daily Events - ${format(day, 'MM/DD/YYYY')}\nToday's Activities`)
         .setColor(0xFF00FF)
@@ -159,6 +165,9 @@ function getEvents(day) {
         const eventDate = new Date(item.eventDate);
         return isSameDay(day, eventDate);
     }).sort(dateSort); 
+}
+function getPledgeText() {
+    return '';
 }
 function processTimezones(item) {
     let startDateTime = new Date(item.eventDate);
@@ -180,32 +189,47 @@ function processEvents(item) {
     item.event.eventDate = item.eventDate;
     item.event.category = category ? category.name : 'None';
 
-    item.event.link = `${process.env.GUILD_SITE}/events/${item.id}?event_instance_id=${item.event_id}`;
+    item.event.link = `${process.env.GUILD_SITE}/events/${item.event_id}?event_instance_id=${item.id}`;
     processTimezones(item.event);
 }
+function getPledges(pageToken) {
+    pageToken = pageToken || '';
+    const uri = `https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDAR_ID}/events?key=${process.env.GAPI_KEY}&pageToken=${pageToken}`;
+    httpGet(uri)
+        .then(data => {
+            pledgeData = pledgeData || data;
+            pledgeItems = pledgeItems.concat(data.items);
+            if (data.nextPageToken) {
+                getPledges(data.nextPageToken);
+            } else {
+                pledgeData.items = pledgeItems.filter(item => item.start).sort(dateSort);
+                pledges = googleCalendarListSort.organizeByDate(pledgeData).filter(isFutureEvent);
+            }
+        });
+}
 function getVEventData() {
-    return getVCalendarData()
+    return httpGet(`${process.env.GUILD_SITE}/events.json`)
         .then(cal => {
             vCalendarData = cal;
-            //vFilteredEvents = vCalendarData.events.filter(isFutureEvent);
-            //vFilteredEvents.sort(dateSort);
         });
 }
 function isFutureEvent(item) {
     const now = new Date();
-    if (item.hasOwnProperty('event_id')) {
-        const eventDate = new Date(item.date);
+    if (item.hasOwnProperty('date')) {
+        const dateString = `${item.date.substr(2, 2)}/${item.date.substr(0, 2)}/${item.date.substr(4, 4)}`;
+        const eventDate = new Date(dateString);
+        console.log(dateString);
         return now.getTime() < eventDate.getTime();        
     }
 };
-function getVCalendarData() {
-    return fetch(`${process.env.GUILD_SITE}/events.json`,
+function httpGet(uri) {
+    return fetch(uri,
         {
             method: 'GET',
             headers: {
-            'Accept': 'application/json'
-        }
-    })
+                'Accept': 'application/json'
+            }
+        })
         .then(response => {
             return response.json();
         })
@@ -218,7 +242,7 @@ function getHelpMessage() {
     .setTitle('Vanquish Bot Commands')
     .setColor(0x750080)
         .setDescription(`
-    \n!cal ###: Show event ### where ### is a number from 0 to ${vFilteredEvents.length}.
+    \n!cal ###: Show event ### where ### is a number from 0 to ${vCalendarData.events.length}.
     \n!channels: Show channels that events will be broadcast to.
     \n!channelinfo: View the current channel's ID and name.
     \n!today: Show today's events.
@@ -239,6 +263,17 @@ function dateSort(item1, item2) {
         }
         if (differenceInMinutes(new Date(item1.eventDate), new Date(item2.eventDate)) < 0) {
             return -1;
+        }
+    } else if (item1.start && item2.start) { 
+        if (item1.start.datetime && item2.start.datetime) {
+            //const dateString1 = `${item1.date.substr(2, 2)}/${item1.date.substr(0, 2)}/${item1.date.substr(4, 4)}`;
+            //const dateString2 = `${item2.date.substr(2, 2)}/${item2.date.substr(0, 2)}/${item2.date.substr(4, 4)}`;
+            if (differenceInDays(new Date(item1.start.datetime), new Date(item2.start.datetime)) >= 0) {
+                return 1;
+            }
+            if (differenceInMinutes(new Date(item1.start.datetime), new Date(item2.start.datetime)) <= 0) {
+                return -1;
+            }
         }
     }
       
@@ -263,4 +298,105 @@ function checkEvents() {
             });
         });
     }
+}
+function loadData() {
+    esoData = {
+        instances:  {
+            "aa": "Aetherian Archive",
+            "hrc": "Hel Ra Citadel",
+            "so": "Sanctum Ophidia",
+            "mol": "Maw of Lorkhaj",
+            "hof": "Halls of Fabrication",
+            "as": "Asylum Sanctorium",
+            "cr": "Cloudrest",
+            "dsa": "Dragonstar Arena",
+            "msa": "Maelstrom Arena",
+            "weekly": "Weekly Trial",
+            "weeklymsa": "Weekly Maelstrom Arena",
+            // Maj
+            "fg2": 'Fungal Grotto II',
+            "sc1": 'Spindleclutch I',
+            "dc2": 'Darkshade Caverns II',
+            "eh1": 'Elden Hollow I',
+            "ws2": 'Wayrest Sewers II',
+            "fg1": 'Fungal Grotto I',
+            "bc2": 'Banished Cells II',
+            "dc1": 'Darkshade Cavern I',
+            "eh2": 'Elden Hollow II',
+            "ws1": 'Wayrest Sewers I',
+            "sc2": 'Spindleclutch II',
+            "bc1": 'Banished Cells I',
+            // Glirion 
+            "coh2": 'Crypt of Hearts II',
+            "coa1": 'City of Ash I',
+            "ti": 'Tempest Island',
+            "bh": 'Blackheart Haven',
+            "arx": 'Arx Corinium',
+            "sw": "Selene's Web",
+            "coa2": 'City of Ash II',
+            "coh1": 'Crypt of Hearts I',
+            "vol": 'Volenfell',
+            "bc": 'Blessed Crucible',
+            "dire": 'Direfrost Keep',
+            "vom": 'Vaults of Madness',
+            // DLC
+            "fh": 'Falkreath Hold',
+            "bf": 'Bloodroot Forge',
+            "cos": 'Cradle of Shadows',
+            "icp": 'Imperial City Prison',
+            "rom": 'Ruins of Mazzatun',
+            "wgt": 'White-Gold Tower',
+            "fl": 'Fang Lair',
+            "sp": 'Scalecaller Peak',
+            "mhk": 'Moon Hunter Keep',
+            "ms": 'March of Sacrifices',
+            "dm": 'Depths of Malatar',
+            "fv": 'Frostvault'
+        },
+        pledgeQuestGiver: ["Maj al-Ragath", "Glirion the Redbeard", "Urgarlag Chief-Bane"],
+        pledges: [{
+            pledgeQuestGiver: 0,
+            instances: ['fg2', 'sc1', 'dc2', 'eh1', 'ws2', 'fg1', 'bc2', 'dc1', 'eh2', 'ws1', 'sc2', 'bc1']
+        }, {
+            pledgeQuestGiver: 1,
+            instances: ['coh2', 'coa1', 'ti', 'bh', 'arx', 'sw', 'coa2', 'coh1', 'vol', 'bc', 'dire', 'vom']
+        }, {
+            pledgeQuestGiver: 2,
+            instances: ['fv', 'icp', 'rom', 'wgt', 'cos', 'bf', 'fh', 'fl', 'sp', 'mhk', 'ms', 'dm']
+        }]
+    };
+}
+function getDailyPledges(dateTime) {
+    // this time calculation has been taken from Seri's code
+    // const resetTime = '11:00pm';
+    // const resetZone = 'America/Los_Angeles';
+    //let elapsed = time - baseTimestamp;
+    //Math.floor(elapsed / 86400);
+
+    // This is hard coded order. Right now there are 12 dungeons on each quest giver. That makes the calculation easy.
+    // That will change with each patch and the calculation for DLC will be different.
+    const baseDate = new Date('05/07/2019');
+    if (!dateTime) {
+        dateTime = Date.now();
+    }
+
+    let diff_rot = differenceInDays(dateTime, baseDate);
+    
+    pledgeText = [];
+    pledgeNext = [];
+    for (var i = 0; i < esoData.pledgeQuestGiver.length;i++){
+        pledgeText.push("* " + getDungeon(i, diff_rot) + " (by " + esoData.pledgeQuestGiver[i] + ")"); 
+        pledgeNext.push(getDungeon(i, diff_rot + 1));
+    }
+        
+    //let remainingH = 23 - Math.floor((elapsed % 86400) / 3600);
+    //let remainingM = 59 - Math.floor(((elapsed % 86400) / 60) % 60);
+    
+    return pledgeText.join('<br/>').concat('<br/><b>Tomorrow:</b><br/>').concat(pledgeNext.join(", "));
+    // + " in " + remainingH + " hours and " + remainingM + " minutes.";
+
+}
+function getDungeon(questGiver, dungeonIndex) {
+    dungeonIndex = dungeonIndex % 12;
+    return esoData.instances[esoData.pledges[questGiver].instances[dungeonIndex]];
 }

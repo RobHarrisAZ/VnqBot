45a6e694aa87c7768c6d28d692bb237f94cdd2af
robharrisaz <monte96_@hotmail.com>
2021-04-22 07:35:03 -0700

    update pledge order

--- modules/data.js
+++ modules/data.js
@@ -1,154 +1,365 @@
 exports.esoData = {
-    instances: {
-        "aa": "Aetherian Archive",
-        "hrc": "Hel Ra Citadel",
-        "so": "Sanctum Ophidia",
-        "mol": "Maw of Lorkhaj",
-        "hof": "Halls of Fabrication",
-        "as": "Asylum Sanctorium",
-        "cr": "Cloudrest",
-        "ss": "Sunspire",        
-        "ka": "Kyne's Aegis",
-        "dsa": "Dragonstar Arena",
-        "msa": "Maelstrom Arena",
-        "weekly": "Weekly Trial",
-        "weeklymsa": "Weekly Maelstrom Arena",
-        // Maj
-        "fg2": { name: 'Fungal Grotto II', link: 'https://youtu.be/fpDzgSwjGCE?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=38' },
-        "sc1": { name: 'Spindleclutch I', link: 'https://youtu.be/CDj17Bwwp3s?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=49' },
-        "dc2": { name: 'Darkshade Caverns II', link: 'https://youtu.be/1zM5MN2XUjk?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=42' },
-        "eh1": { name: 'Elden Hollow I', link: 'https://youtu.be/rNMpX5B8IOA?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=41' },
-        "ws2": { name: 'Wayrest Sewers II', link: 'https://youtu.be/eMzfUAeDuhE?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=35' },
-        "fg1": { name: 'Fungal Grotto I', link: 'https://youtu.be/vvHCjVhqDvI?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=76' },
-        "bc2": { name: 'Banished Cells II', link: 'https://youtu.be/lHgoKTIsOak?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=45' },
-        "dc1": { name: 'Darkshade Cavern I', link: 'https://youtu.be/xDp9D0hOQlI?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=63' },
-        "eh2": { name: 'Elden Hollow II', link: 'https://youtu.be/5rnm1kvpL2A?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=32' },
-        "ws1": { name: 'Wayrest Sewers I', link: 'https://youtu.be/0zH11XetbiQ?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39' },
-        "sc2": { name: 'Spindleclutch II', link: 'https://youtu.be/kB2NbTau5m8?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=36' },
-        "bc1": { name: 'Banished Cells I', link: 'https://youtu.be/qXOXfxhjTso?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=96' },
-        // Glirion 
-        "coh2": { name: 'Crypt of Hearts II', link: 'https://youtu.be/w3GoDnRQBbM?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=37' },
-        "coa1": { name: 'City of Ash I', link: 'https://youtu.be/3mNGHckmZEc?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=41' },
-        "ti": { name: 'Tempest Island', link: 'https://youtu.be/DDIbXPXUUkk?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=35' },
-        "bh": { name: 'Blackheart Haven', link: 'https://youtu.be/h8Zl4iqq3I0?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39' },
-        "arx": { name: 'Arx Corinium', link: 'https://youtu.be/Un8ex_Uwt1g?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=52' },
-        "sw": { name: 'Selene\'s Web', link: 'https://youtu.be/YtKMJxq9sZA?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=32' },
-        "coa2": { name: 'City of Ash II', link: 'https://youtu.be/Kh_y9pK1yK8?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39' },
-        "coh1": { name: 'Crypt of Hearts I', link: 'https://youtu.be/biP9yE7zOfE?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=45' },
-        "vol": { name: 'Volenfell', link: 'https://youtu.be/eVTf5jl9i7c?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=42' },
-        "bc": { name: 'Blessed Crucible', link: 'https://youtu.be/SJf2g9oq8oQ?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=35' },
-        "dire": { name: 'Direfrost Keep', link: 'https://youtu.be/RBPYjLk1e3c?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=41' },
-        "vom": { name: 'Vaults of Madness', link: 'https://youtu.be/ZcNi6PsrGD8?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=38' },
-        // DLC
-        "fh": { name: 'Falkreath Hold', link: 'https://youtu.be/VCzikSum7x8?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=32' },
-        "bf": { name: 'Bloodroot Forge', link: 'https://youtu.be/_ztZvtd77Mw?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=31' },
-        "cos": { name: 'Cradle of Shadows', link: 'https://youtu.be/cRrL40vVn8Q?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=34' },
-        "icp": { name: 'Imperial City Prison', link: 'https://youtu.be/0rQeOQyPNqM?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39' },
-        "rom": { name: 'Ruins of Mazzatun', link: 'https://youtu.be/1vZU0ZcG7wc?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=37' },
-        "wgt": { name: 'White-Gold Tower', link: 'https://youtu.be/neGKIiJLSWc?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39' },
-        "fl": { name: 'Fang Lair', link: 'https://youtu.be/-z0W5rS-lDU?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=34' },
-        "sp": { name: 'Scalecaller Peak', link: 'https://youtu.be/T-0bx6SUxIo?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=40' },
-        "mhk": { name: 'Moon Hunter Keep', link: 'https://youtu.be/rUi-hQFMy9s?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39' },
-        "ms": { name: 'March of Sacrifices', link: 'https://youtu.be/sxLgJyIUPmk?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=36' },
-        "dm": { name: 'Depths of Malatar', link: 'https://youtu.be/HS4-WfimNQI?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=36' },
-        "fv": { name: 'Frostvault', link: 'https://youtu.be/CXSw6r0hAAg?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=37' },
-        "mf": { name: 'Moongrave Fane', link: 'https://www.youtube.com/watch?v=HSQCX7WF_GI' },
-        "lom": { name: 'Lair of Maarselok', link: 'https://www.youtube.com/watch?v=4Yy2kCWKb8o'},
-        "ug": { name: 'Unhallowed Grave', link: 'https://www.youtube.com/watch?v=D6xHDg7CcSs'},
-        "ir": { name: 'Icereach', link: 'https://www.youtube.com/watch?v=KVZp83mLQ-4'},
-        "sg": { name: 'Stone Garden', link: '#'},
-        "ct": { name: 'Castle Thorn', link: '#'},
-        "tc": { name: 'The Cauldron', link: '#'},
-        "bdv": { name: 'Black Drake Villa', link: '#'}
+  instances: {
+    aa: "Aetherian Archive",
+    hrc: "Hel Ra Citadel",
+    so: "Sanctum Ophidia",
+    mol: "Maw of Lorkhaj",
+    hof: "Halls of Fabrication",
+    as: "Asylum Sanctorium",
+    cr: "Cloudrest",
+    ss: "Sunspire",
+    ka: "Kyne's Aegis",
+    dsa: "Dragonstar Arena",
+    msa: "Maelstrom Arena",
+    weekly: "Weekly Trial",
+    weeklymsa: "Weekly Maelstrom Arena",
+    // Maj
+    fg2: {
+      name: "Fungal Grotto II",
+      link:
+        "https://youtu.be/fpDzgSwjGCE?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=38",
     },
-    pledgeQuestGiver: ["Maj al-Ragath", "Glirion the Redbeard", "Urgarlag Chief-Bane"],
-    pledges: [{
-        pledgeQuestGiver: 0,
-        instances: ['dc1', 'eh2', 'ws1', 'sc2', 'bc1', 'fg2', 'sc1', 'dc2', 'eh1', 'ws2', 'fg1', 'bc2']
-    }, {
-        pledgeQuestGiver: 1,
-        instances: ['coh1', 'vol', 'bc', 'dire', 'vom', 'coh2', 'coa1', 'ti', 'bh', 'arx', 'sw', 'coa2']
-    }, {
-        pledgeQuestGiver: 2,
-        instances: ['ir', 'ug', 'sg', 'ct', 'bdv', 'tc', 'icp', 'rom', 'wgt', 'cos', 'bf', 'fh', 'fl', 'sp', 'mhk', 'ms', 'dm', 'fv', 'mf', 'lom']
-    }], 
-    trials: ['aa', 'hrc', 'so', 'mol', 'hof', 'as', 'cr', 'ss', 'ka' ],
-    firstBosses: [
-        { name: 'Lightning Storm Atronach', trial: 'aa' },
-        { name: 'Ra Kotu', trial: 'hrc' },
-        { name: 'Mantacora', trial: 'so' },
-        { name: 'Zhaj\'hassa', trial: 'mol' },
-        { name: 'Hunter Killers', trial: 'hof' },
-        { name: 'Saint Llothis the Pious', trial: 'as' },
-        { name: 'Saint Felms the Bold', trial: 'as' },
-        { name: 'Shade of Galanwe', trial: 'cr' },
-        { name: 'Shade of Relequen', trial: 'cr' },
-        { name: 'Shade of Siroria', trial: 'cr' },
-        { name: 'Yolnahkrin', trial: 'ss' },        
-        { name: 'Lokkestiiz', trial: 'ss' }
-    ],
-    zones: [
-        { id: 0, name: `Auridon` },
-        { id: 1, name: `Glenumbra` },
-        { id: 2, name: `Rivenspire` },
-        { id: 3, name: `Stormhaven` },
-        { id: 4, name: `Bangkorai` },
-        { id: 5, name: `Alik'r` },
-        { id: 6, name: `Malabal Tor` },
-        { id: 7, name: `Greenshade` },
-        { id: 8, name: `Grahtwood` },
-        { id: 9, name: `Reaper's March` },
-        { id: 10, name: `Shadowfen` },
-        { id: 11, name: `Deshaan` },
-        { id: 12, name: `Stonefalls` },
-        { id: 13, name: `Eastmarch` },
-        { id: 14, name: `The Rift` },
-        { id: 15, name: `Craglorn` },
-        { id: 16, name: `Coldharbour` },
-        { id: 17, name: `Cyrodil (EP Area)` },
-        { id: 18, name: `Cyrodil (AD Area)` },
-        { id: 19, name: `Cyrodil (DC Area)` }
-    ],
-    spdZones: [
-        { id: 0, zones: [12]},
-        { id: 1, zones: [11]},
-        { id: 2, zones: [10]},
-        { id: 3, zones: [13]},
-        { id: 4, zones: [14]},
-        { id: 5, zones: [17]},
-        { id: 6, zones: [0]},
-        { id: 7, zones: [8]},
-        { id: 8, zones: [7]},
-        { id: 9, zones: [6]},
-        { id: 10, zones: [9]},
-        { id: 11, zones: [18]},
-        { id: 12, zones: [1]},
-        { id: 13, zones: [3]},
-        { id: 14, zones: [2]},
-        { id: 15, zones: [4]},
-        { id: 16, zones: [5]},
-        { id: 17, zones: [19]},
-        { id: 18, zones: [16]}
-    ],
-    mnmActivities: [
-        { id: 0, name: `Imperial Sewers`},
-        { id: 1, name: `Imperial City`},
-        { id: 2, name: `Wrothgar`},
-        { id: 3, name: `Vvardenfell`},
-        { id: 4, name: `Summerset`},
-        { id: 5, name: `Northern Elsweyr`},
-        { id: 6, name: `Western Skyrim`},
-        { id: 7, name: `Hew's Bane, Gold Coast & Clockwork City`},
-        { id: 8, name: `Murkmire`},
-        { id: 9, name: `Southern Elsweyr`},
-        { id: 10, name: `The Reach`}
-    ],
-    smsZones: [
-        { id: 0, zones: [0, 1, 2] },
-        { id: 1, zones: [3, 4, 5] },
-        { id: 2, zones: [6, 7, 8] },
-        { id: 3, zones: [9, 10, 11] },
-        { id: 4, zones: [12, 13, 14] },
-        { id: 5, zones: [15, 16] }
-    ]
+    sc1: {
+      name: "Spindleclutch I",
+      link:
+        "https://youtu.be/CDj17Bwwp3s?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=49",
+    },
+    dc2: {
+      name: "Darkshade Caverns II",
+      link:
+        "https://youtu.be/1zM5MN2XUjk?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=42",
+    },
+    eh1: {
+      name: "Elden Hollow I",
+      link:
+        "https://youtu.be/rNMpX5B8IOA?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=41",
+    },
+    ws2: {
+      name: "Wayrest Sewers II",
+      link:
+        "https://youtu.be/eMzfUAeDuhE?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=35",
+    },
+    fg1: {
+      name: "Fungal Grotto I",
+      link:
+        "https://youtu.be/vvHCjVhqDvI?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=76",
+    },
+    bc2: {
+      name: "Banished Cells II",
+      link:
+        "https://youtu.be/lHgoKTIsOak?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=45",
+    },
+    dc1: {
+      name: "Darkshade Cavern I",
+      link:
+        "https://youtu.be/xDp9D0hOQlI?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=63",
+    },
+    eh2: {
+      name: "Elden Hollow II",
+      link:
+        "https://youtu.be/5rnm1kvpL2A?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=32",
+    },
+    ws1: {
+      name: "Wayrest Sewers I",
+      link:
+        "https://youtu.be/0zH11XetbiQ?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39",
+    },
+    sc2: {
+      name: "Spindleclutch II",
+      link:
+        "https://youtu.be/kB2NbTau5m8?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=36",
+    },
+    bc1: {
+      name: "Banished Cells I",
+      link:
+        "https://youtu.be/qXOXfxhjTso?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=96",
+    },
+    // Glirion
+    coh2: {
+      name: "Crypt of Hearts II",
+      link:
+        "https://youtu.be/w3GoDnRQBbM?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=37",
+    },
+    coa1: {
+      name: "City of Ash I",
+      link:
+        "https://youtu.be/3mNGHckmZEc?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=41",
+    },
+    ti: {
+      name: "Tempest Island",
+      link:
+        "https://youtu.be/DDIbXPXUUkk?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=35",
+    },
+    bh: {
+      name: "Blackheart Haven",
+      link:
+        "https://youtu.be/h8Zl4iqq3I0?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39",
+    },
+    arx: {
+      name: "Arx Corinium",
+      link:
+        "https://youtu.be/Un8ex_Uwt1g?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=52",
+    },
+    sw: {
+      name: "Selene's Web",
+      link:
+        "https://youtu.be/YtKMJxq9sZA?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=32",
+    },
+    coa2: {
+      name: "City of Ash II",
+      link:
+        "https://youtu.be/Kh_y9pK1yK8?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39",
+    },
+    coh1: {
+      name: "Crypt of Hearts I",
+      link:
+        "https://youtu.be/biP9yE7zOfE?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=45",
+    },
+    vol: {
+      name: "Volenfell",
+      link:
+        "https://youtu.be/eVTf5jl9i7c?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=42",
+    },
+    bc: {
+      name: "Blessed Crucible",
+      link:
+        "https://youtu.be/SJf2g9oq8oQ?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=35",
+    },
+    dire: {
+      name: "Direfrost Keep",
+      link:
+        "https://youtu.be/RBPYjLk1e3c?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=41",
+    },
+    vom: {
+      name: "Vaults of Madness",
+      link:
+        "https://youtu.be/ZcNi6PsrGD8?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=38",
+    },
+    // DLC
+    fh: {
+      name: "Falkreath Hold",
+      link:
+        "https://youtu.be/VCzikSum7x8?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=32",
+    },
+    bf: {
+      name: "Bloodroot Forge",
+      link:
+        "https://youtu.be/_ztZvtd77Mw?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=31",
+    },
+    cos: {
+      name: "Cradle of Shadows",
+      link:
+        "https://youtu.be/cRrL40vVn8Q?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=34",
+    },
+    icp: {
+      name: "Imperial City Prison",
+      link:
+        "https://youtu.be/0rQeOQyPNqM?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39",
+    },
+    rom: {
+      name: "Ruins of Mazzatun",
+      link:
+        "https://youtu.be/1vZU0ZcG7wc?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=37",
+    },
+    wgt: {
+      name: "White-Gold Tower",
+      link:
+        "https://youtu.be/neGKIiJLSWc?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39",
+    },
+    fl: {
+      name: "Fang Lair",
+      link:
+        "https://youtu.be/-z0W5rS-lDU?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=34",
+    },
+    sp: {
+      name: "Scalecaller Peak",
+      link:
+        "https://youtu.be/T-0bx6SUxIo?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=40",
+    },
+    mhk: {
+      name: "Moon Hunter Keep",
+      link:
+        "https://youtu.be/rUi-hQFMy9s?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=39",
+    },
+    ms: {
+      name: "March of Sacrifices",
+      link:
+        "https://youtu.be/sxLgJyIUPmk?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=36",
+    },
+    dm: {
+      name: "Depths of Malatar",
+      link:
+        "https://youtu.be/HS4-WfimNQI?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=36",
+    },
+    fv: {
+      name: "Frostvault",
+      link:
+        "https://youtu.be/CXSw6r0hAAg?list=PL8AZn0Ie30d4d35rT1YhmcHuyfhLHsii7&t=37",
+    },
+    mf: {
+      name: "Moongrave Fane",
+      link: "https://www.youtube.com/watch?v=HSQCX7WF_GI",
+    },
+    lom: {
+      name: "Lair of Maarselok",
+      link: "https://www.youtube.com/watch?v=4Yy2kCWKb8o",
+    },
+    ug: {
+      name: "Unhallowed Grave",
+      link: "https://www.youtube.com/watch?v=D6xHDg7CcSs",
+    },
+    ir: {
+      name: "Icereach",
+      link: "https://www.youtube.com/watch?v=KVZp83mLQ-4",
+    },
+    sg: { name: "Stone Garden", link: "#" },
+    ct: { name: "Castle Thorn", link: "#" },
+    tc: { name: "The Cauldron", link: "#" },
+    bdv: { name: "Black Drake Villa", link: "#" },
+  },
+  pledgeQuestGiver: [
+    "Maj al-Ragath",
+    "Glirion the Redbeard",
+    "Urgarlag Chief-Bane",
+  ],
+  pledges: [
+    {
+      pledgeQuestGiver: 0,
+      instances: [
+        "dc1",
+        "eh2",
+        "ws1",
+        "sc2",
+        "bc1",
+        "fg2",
+        "sc1",
+        "dc2",
+        "eh1",
+        "ws2",
+        "fg1",
+        "bc2",
+      ],
+    },
+    {
+      pledgeQuestGiver: 1,
+      instances: [
+        "coh1",
+        "vol",
+        "bc",
+        "dire",
+        "vom",
+        "coh2",
+        "coa1",
+        "ti",
+        "bh",
+        "arx",
+        "sw",
+        "coa2",
+      ],
+    },
+    {
+      pledgeQuestGiver: 2,
+      instances: [
+        "mf",
+        "lom",
+        "ir",
+        "ug",
+        "sg",
+        "ct",
+        "bdv",
+        "tc",
+        "icp",
+        "rom",
+        "wgt",
+        "cos",
+        "bf",
+        "fh",
+        "fl",
+        "sp",
+        "mhk",
+        "ms",
+        "dm",
+        "fv",
+      ],
+    },
+  ],
+  trials: ["aa", "hrc", "so", "mol", "hof", "as", "cr", "ss", "ka"],
+  firstBosses: [
+    { name: "Lightning Storm Atronach", trial: "aa" },
+    { name: "Ra Kotu", trial: "hrc" },
+    { name: "Mantacora", trial: "so" },
+    { name: "Zhaj'hassa", trial: "mol" },
+    { name: "Hunter Killers", trial: "hof" },
+    { name: "Saint Llothis the Pious", trial: "as" },
+    { name: "Saint Felms the Bold", trial: "as" },
+    { name: "Shade of Galanwe", trial: "cr" },
+    { name: "Shade of Relequen", trial: "cr" },
+    { name: "Shade of Siroria", trial: "cr" },
+    { name: "Yolnahkrin", trial: "ss" },
+    { name: "Lokkestiiz", trial: "ss" },
+  ],
+  zones: [
+    { id: 0, name: `Auridon` },
+    { id: 1, name: `Glenumbra` },
+    { id: 2, name: `Rivenspire` },
+    { id: 3, name: `Stormhaven` },
+    { id: 4, name: `Bangkorai` },
+    { id: 5, name: `Alik'r` },
+    { id: 6, name: `Malabal Tor` },
+    { id: 7, name: `Greenshade` },
+    { id: 8, name: `Grahtwood` },
+    { id: 9, name: `Reaper's March` },
+    { id: 10, name: `Shadowfen` },
+    { id: 11, name: `Deshaan` },
+    { id: 12, name: `Stonefalls` },
+    { id: 13, name: `Eastmarch` },
+    { id: 14, name: `The Rift` },
+    { id: 15, name: `Craglorn` },
+    { id: 16, name: `Coldharbour` },
+    { id: 17, name: `Cyrodil (EP Area)` },
+    { id: 18, name: `Cyrodil (AD Area)` },
+    { id: 19, name: `Cyrodil (DC Area)` },
+  ],
+  spdZones: [
+    { id: 0, zones: [12] },
+    { id: 1, zones: [11] },
+    { id: 2, zones: [10] },
+    { id: 3, zones: [13] },
+    { id: 4, zones: [14] },
+    { id: 5, zones: [17] },
+    { id: 6, zones: [0] },
+    { id: 7, zones: [8] },
+    { id: 8, zones: [7] },
+    { id: 9, zones: [6] },
+    { id: 10, zones: [9] },
+    { id: 11, zones: [18] },
+    { id: 12, zones: [1] },
+    { id: 13, zones: [3] },
+    { id: 14, zones: [2] },
+    { id: 15, zones: [4] },
+    { id: 16, zones: [5] },
+    { id: 17, zones: [19] },
+    { id: 18, zones: [16] },
+  ],
+  mnmActivities: [
+    { id: 0, name: `Imperial Sewers` },
+    { id: 1, name: `Imperial City` },
+    { id: 2, name: `Wrothgar` },
+    { id: 3, name: `Vvardenfell` },
+    { id: 4, name: `Summerset` },
+    { id: 5, name: `Northern Elsweyr` },
+    { id: 6, name: `Western Skyrim` },
+    { id: 7, name: `Hew's Bane, Gold Coast & Clockwork City` },
+    { id: 8, name: `Murkmire` },
+    { id: 9, name: `Southern Elsweyr` },
+    { id: 10, name: `The Reach` },
+  ],
+  smsZones: [
+    { id: 0, zones: [0, 1, 2] },
+    { id: 1, zones: [3, 4, 5] },
+    { id: 2, zones: [6, 7, 8] },
+    { id: 3, zones: [9, 10, 11] },
+    { id: 4, zones: [12, 13, 14] },
+    { id: 5, zones: [15, 16] },
+  ],
 };
